d7dd17794c7271c8dd7bf18f88e1c94baec24534
inayatullahaaxioned <inayatullaha@axioned.com>
2022-11-10 15:12:18 +0530

    navbar and to-top btn js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
 /* Author:
 Inayatullah
 */
+//hamburger js
+const hamburger = document.querySelector('.hamburger'),
+  navbar = document.querySelector('.nav');
 
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('active');
+  navbar.classList.toggle('active');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//to top btn js
+const scrollTop = document.querySelector(".top-btn");
+scrollTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
